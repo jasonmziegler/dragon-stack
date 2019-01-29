2ecf3f5e393e92e5632fe80d917f22612a817a45
jasonmziegler <jason@insightfulautomation.com>
2019-01-29 13:05:44 -0600

    add database configuration and databasePool

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const GenerationEngine = require('./generation/engine');
+const dragonRouter = require('./api/dragon');
+const generationRouter = require('./api/generation');
 
 const app = express();
 const engine = new GenerationEngine();
 
+app.locals.engine = engine;
+
+app.use('/dragon', dragonRouter);
+app.use('/generation', generationRouter);
+
 engine.start();
 
-app.get('/dragon/new',(req, res) => {
-    res.json({ dragon: engine.generation.newDragon( )});
-});
 
 module.exports = app;
