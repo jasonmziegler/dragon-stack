b7be8bf2511a3c4eacc3a6029f2504473c044894
jasonmziegler <jason@insightfulautomation.com>
2019-02-07 21:55:06 -0600

    add redux to react frontend

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 
-class Generation extends Component {
-     render() {
-         const generation = {generationId: 999, expiration: '2020-05-01'};
-         return (
-             <div>
-                 <h3>Generation { generation.generationId }. Expires on:</h3>
-                 <h4>{new Date(generation.expiration).toString()}</h4>
+const DEFAULT_GENERATION = { generationId: '', expiration: ''}
 
-             </div>
-         )
-     }
+const MINIMUM_DELAY = 3000;
+
+class Generation extends Component {
+    state = {
+        generation: DEFAULT_GENERATION
+    };
+
+    timer = null;
+
+    componentDidMount() {
+        this.fetchNextGeneration();
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer);
+    }
+
+    fetchGeneration = () => {
+        fetch('http://localhost:3000/generation')
+        .then(response => response.json())
+        .then(json => { 
+            //console.log('json', json);
+
+            this.setState({generation: json.generation });
+        
+        })
+        .catch(error => console.error('error', error));
+    };
+
+    fetchNextGeneration = () => { 
+        this.fetchGeneration();
+
+        let delay = new Date(this.state.generation.expiration).getTime() - new Date().getTime();
+
+        if (delay < MINIMUM_DELAY) {
+            delay = MINIMUM_DELAY;
+        }
+
+        this.timer = setTimeout(() => this.fetchNextGeneration(), delay);
+    }
+
+    render() {
+         const { generation } = this.state;
+
+        return (
+            <div>
+                <h3>Generation { generation.generationId }. Expires on:</h3>
+                <h4>{new Date(generation.expiration).toString()}</h4>
+
+            </div>
+        )
+    }
 }
 
 export default Generation;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import Generation from './components/Generation';
+import Dragon from './components/Dragon';
+
+import './index.css';
+
+const DEFAULT_GENERATION =  { generationId: '', expiration: '' };;
+
+const generationReducer = () => {
+    return { generation: DEFAULT_GENERATION };
+}
+
+const store = createStore(generationReducer);
+
+console.log('store', store);
+console.log('store.getState()',store.getState());
 
 ReactDOM.render(
 <div>
-<h2>Dragon Stack from React</h2>
+<h2>Dragon Stack</h2>
 <Generation />
+<Dragon />
 </div>
 , document.getElementById('root'));
 
