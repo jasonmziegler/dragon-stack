323c227f424379fbc00089badab184e9880d4347
jasonmziegler <jason@insightfulautomation.com>
2019-02-13 22:14:41 -0600

    add session and login

---
+++ backend/app/api/account.js
@@ -0,0 +1,52 @@
+const { Router } = require('express');
+const AccountTable = require('../account/table');
+const { hash } = require('../account/helper');
+const Session = require('../account/session');
+const { setSession } = require('./helper');
+
+const router = new Router();
+
+router.post('/signup', (req, res, next) => {
+    const { username, password } = req.body;
+    const usernameHash = hash(username);
+    const passwordHash = hash(password);
+
+    AccountTable.getAccount({ usernameHash })
+        .then(({ account }) => {
+            if (!account) {
+                return AccountTable.storeAccount({ usernameHash, passwordHash })
+            } else {
+                const error = new Error('This username has already been taken');
+
+                error.statusCode = 409;
+                
+                throw error;
+            }
+        })
+        .then(() => {
+            return setSession({ username, res});
+        })
+        .then(({ message }) => res.json({ message }))
+        .catch(error => next(error));
+});
+
+router.post('/login', (req, res, next) => {
+    const {username, password } = req.body;
+
+    AccountTable.getAccount({ usernameHash: hash(username) })
+        .then(({ account }) => {
+            if (account && account.passwordHash === hash(password)) {
+                return setSession({ username, res })
+            } else {
+                const error = new Error('Incorrect username/password');
+
+                error.statusCode = 409;
+
+                throw error;
+            }
+        })
+        .then(({ message }) => res.json({message}))
+        .catch((error) => next(error));
+});
+
+module.exports = router;
